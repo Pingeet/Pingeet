ff47ca2b1e1236c7d642811c439b29699fc9ccf1
Robin Paul <paul.robin@etu.unistra.fr>
2015-04-19 11:37:11 +0200

    add the date in a new geet

--- ressources/accueil/acc_app.js
+++ ressources/accueil/acc_app.js
@@ -1,11 +1,8 @@
-// js/geetList.js
 'use strict';
-/**
- *  statement of demoApp
- */
 var Acc_App = angular.module('Acc_App', [
     'geetList',
-    'clockApp'
+    'clockApp',
+    //'geetData'
 ]);
 
 var geetList = angular.module('geetList', []);
@@ -15,14 +12,23 @@ geetList.controller('geet_Ctrl', ['$scope',
         var geetS = $scope.geetS = [];
         $scope.addGeet = function ()
         {   
+            var time=gettime();
             var user="babouin";
             var newGeet = $scope.newGeet.trim();// .trim() allow to remote the useless spaces at the beginning and the end of the characters string   
             if (!newGeet.length)                // avoid empty geetS
             {return;}
             geetS.unshift(
             {// add the geet at the start of geetS 
-                title: newGeet,
-                user: user
+                msg: newGeet,
+                user: user,
+                date: {
+                        day:time.day,
+                        month:time.month,
+                        year:time.year,
+                        hours:time.hours,
+                        minutes:time.minutes,
+                        seconds:time.seconds
+                      }
             });
             $scope.newGeet = '';                // re-set newGeet
         };
