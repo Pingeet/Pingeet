5c92444d29d6d0a838ea7a77940f2c3ddddc7628
Robin Paul <paul.robin@etu.unistra.fr>
2015-04-19 11:39:59 +0200

    no change in functions, only structure of the document to still work with the changes of acc_app

--- ressources/general_app.js
+++ ressources/general_app.js
@@ -1,16 +1,22 @@
+'use strict';
+var currentdate = new Date();
+var gettime = function ()
+{
+    var time=
+    {
+        day: currentdate.getDate(),
+        month: currentdate.getMonth(),
+        year: currentdate.getFullYear(),
+        hours: currentdate.getHours(),
+        minutes: currentdate.getMinutes(),
+        seconds: currentdate.getSeconds()
+    };
+    return time;
+};
 var clockApp = angular.module('clockApp', []);
     clockApp.controller('dateCtrl', ['$scope',
         function($scope)
         {
         // Return date and current time
-            var currentdate = new Date(); 
-            $scope.time=
-            {
-            day: currentdate.getDate(),
-            month: currentdate.getMonth(),
-            year: currentdate.getFullYear(),
-            hours: currentdate.getHours(),
-            minutes: currentdate.getMinutes(),
-            seconds: currentdate.getSeconds()
-            };
+            $scope.angtime=gettime();
         }]);
