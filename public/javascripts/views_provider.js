ef9814ebbd05a83c83ff1aa857d64fb677b9f74c
Robin Paul <paul.robin@etu.unistra.fr>
2015-05-11 23:57:32 +0200

    set new src for angular, and time.js/ add a little script to set the href for  , in views_provider : rm usless stuff

--- public/javascripts/views_provider.js
+++ public/javascripts/views_provider.js
@@ -1,31 +1,21 @@
+(function(angular) {
 'use strict';
-
-var view_pro = angular.module('view_pro', []);
-view_pro.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-
-        $routeProvider
+angular.module('view_pro', ['ngRoute'])
+    .config(['$routeProvider', '$locationProvider',
+        function ($routeProvider, $locationProvider) {
+            $routeProvider
             .when('/login', {
                 templateUrl: '/html/login.html',
                 controller: 'viewCtrl'
             })
 
-        .when('/', {
-            templateUrl: '/html/home.html',
-            controller: 'viewCtrl'
-        })
+            .when('/', {
+                templateUrl: '/html/home.html',
+                controller: 'viewCtrl'
+            })
 
-        .otherwise({
-            redirectTo: '/login'
-        });
-        $locationProvider.html5Mode(true);
-}])
-    .controller('MainCtrl', ['$route', '$routeParams', '$location',
-  function ($route, $routeParams, $location) {
-            this.$route = $route;
-            this.$location = $location;
-            this.$routeParams = $routeParams;
-}])
-    .controller('viewCtrl', ['$routeParams', function ($routeParams) {
-        this.name = "viewCtrl";
-        this.params = $routeParams;
+            .otherwise({
+                redirectTo: '/login'
+            });
 }]);
+})(window.angular);
