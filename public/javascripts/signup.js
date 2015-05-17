6a04f14abfdf08278c106c3e3fa377599671ee3a
root <root@localhost.localdomain>
2015-05-17 12:15:26 +0200

    création d'un module userService et mise en place des post

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,4 +1,9 @@
 'use strict';
+var userService = angular.module('userService', [
+'addUser',
+'getUser'
+]);
+
 var addUser = angular.module('addUser', []);
 addUser.controller('addUser_Ctrl', ['$scope', '$http',
     function ($scope, $http) {
@@ -13,15 +18,36 @@ addUser.controller('addUser_Ctrl', ['$scope', '$http',
                 login: username,
                 password: password
             };
-            $http.post('/create', $scope.newUser).success(function (data) {
+            $http.post('/user/create', $scope.newUser).success(function (data) {
                 if (data == 'err') {
-                    alert("Désolé un problème est survenu lors de l 'enregistrement");
+                    alert("Error");
                 } else {
-                    alert("La fiche a bien été enregistrée");
+                    alert("Success");
                 }
                 $scope.perso = {};
-            }) 
+            })
             $scope.username = '';
             $scope.password = '';
         };
-                }]);
+    }
+]);
+
+var getUser = angular.module('getUser', []);
+getUser.controller('getUser_Ctrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $scope.findUser = function () {
+            $scope.logpass = {
+                login: $scope.username.trim(),
+                secretPW: $scope.password
+            }
+            $http.post('/user/find', $scope.logpass)
+                .success(function (data) {
+                    if (data != 'err') {
+                        $scope.user = data;
+                    }
+                    $scope.username = '';
+                    $scope.password = '';
+                })
+        }
+    }
+]);
